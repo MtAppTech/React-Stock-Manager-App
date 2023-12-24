a02d2a8c536696b5b617bd41e1af0cdaefec72c5
murat <yazilimgenel@gmail.com>
2023-12-24 23:52:13 +0100

    mac Formik-yup -form create

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,8 +8,34 @@ import { Link } from "react-router-dom";
 import { Box } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
-import { Formik } from "formik";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string() // Defined values must be the same as the form
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Notwendig!"),
+  username: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Notwendig!"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Notwendig!"),
+  email: Yup.string().email().required("Notwendig!"),
+  password: Yup.string()
+    .min(8, "Er muss mindestens 8 Zeichen lang sein!")
+    .max(50, "Er darf maximal 50 Zeichen lang sein!")
+    .matches(/\d+/, "Muss mindestens eine Ziffer enthalten!")
+    .matches(/[A-Z]/, "Muss mindestens einen Großbuchstaben enthalten!")
+    .matches(/[a-z]/, "Muss mindestens einen Kleinbuchstaben enthalten!")
+    .matches(/[@$!%*?&]+/, "Muss mindestens ein Sonderzeichen enthalten!")
+    .required("Notwendig!"),
+});
 const Register = () => {
   return (
     <Container maxWidth="lg">
@@ -44,6 +70,7 @@ const Register = () => {
           >
             Register
           </Typography>
+
           <Formik
             initialValues={{
               username: "",
@@ -52,7 +79,77 @@ const Register = () => {
               firstName: "",
               lastName: "",
             }}
-          ></Formik>
+            validationSchema={SignupSchema}
+            onSubmit={(values, actions) => {
+              // same shape as initial values
+              console.log(values);
+            }}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+              /* and other goodies */
+            }) => (
+              <Form>
+                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                  <TextField
+                    id="username"
+                    name="username" //matching based on formal name attribute.
+                    label="Username"
+                    value={values.username}
+                    onChange={handleChange}
+                    onBlur={handleBlur} // event indicating that the user left the input field
+                    helperText={touched.username && errors.username}
+                    error={touched.username && Boolean(errors.username)}
+                    // touched also detects whether the user clicked on the input or not
+                  />
+                  <TextField
+                    id="firstName"
+                    name="firstName"
+                    label="First Name"
+                    value={values.firstName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={touched.firstName && errors.firstName}
+                    error={touched.firstName && Boolean(errors.firstName)}
+                  />
+                  <TextField
+                    id="lastName"
+                    name="lastName"
+                    label="Last Name"
+                    value={values.lastName}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    helperText={touched.lastName && errors.lastName}
+                    error={touched.lastName && Boolean(errors.lastName)}
+                  />
+                  <TextField
+                    id="email"
+                    label="Email"
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                  />
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    disabled={isSubmitting}
+                  >
+                    Sign Up
+                  </Button>
+                </Box>
+              </Form>
+            )}
+          </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
             <Link to="/">Already have an account? Sign in</Link>
