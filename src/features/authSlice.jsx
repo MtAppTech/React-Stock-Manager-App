ff7c536d8f9c1dd17ad0619ca4759f5878d0906c
murat <yazilimgenel@gmail.com>
2024-01-01 22:57:11 +0100

    mac login page create

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -20,6 +20,18 @@ const authSlice = createSlice({
       state.currentUser = payload.data.username;
       state.token = payload.token;
     },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.currentUser = payload?.user?.username;
+      state.isAdmin = payload?.user?.isAdmin;
+      state.token = payload?.token;
+    },
+    logoutSuccess: (state) => {
+      state.loading = false;
+      state.currentUser = null;
+      state.isAdmin = false;
+      state.token = null;
+    },
     fetchFail: (state) => {
       state.loading = false;
       state.error = true;
@@ -27,5 +39,11 @@ const authSlice = createSlice({
   },
 });
 
-export const { fetchStart, registerSuccess, fetchFail } = authSlice.actions;
+export const {
+  fetchStart,
+  registerSuccess,
+  logoutSuccess,
+  loginSuccess,
+  fetchFail,
+} = authSlice.actions;
 export default authSlice.reducer;
--- src/hooks/useAuthCall.js
+++ src/hooks/useAuthCall.js
@@ -1,11 +1,21 @@
-import { useDispatch } from "react-redux";
-import { fetchFail, fetchStart, registerSuccess } from "../features/authSlice";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchFail,
+  fetchStart,
+  registerSuccess,
+  loginSuccess,
+  logoutSuccess,
+} from "../features/authSlice";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { toastErrorNotify, toastSuccessNotify } from "..//helper/ToastNotify";
 
 const useAuthCall = () => {
   const dispatch = useDispatch(); //use to define the fetchStart function
   const navigate = useNavigate();
+  const { token } = useSelector((store) => store.auth);
+
   const register = async (userInfo) => {
     dispatch(fetchStart());
     try {
@@ -20,8 +30,41 @@ const useAuthCall = () => {
       dispatch(fetchFail());
     }
   };
-
-  return { register }; // use this to open the custom hook function globally
+  const login = async (userInfo) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axios.post(
+        `https://17107.fullstack.clarusway.com/auth/login/`,
+        userInfo
+      );
+      dispatch(loginSuccess(data));
+      toastSuccessNotify("Login performed");
+      navigate("/stock");
+      console.log(data);
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+      toastErrorNotify("Login can not be performed");
+    }
+  };
+  const logout = async () => {
+    dispatch(fetchStart());
+    try {
+      await axios.get(`https://17107.fullstack.clarusway.com/auth/logout/`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
+      dispatch(logoutSuccess());
+      toastSuccessNotify("Logout performed");
+      navigate("/");
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+      toastErrorNotify("Logout can not be performed");
+    }
+  };
+  return { register, login, logout }; // use this to open the custom hook function globally
 };
 
 export default useAuthCall;
